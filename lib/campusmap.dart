6dfac18468a580c20451a7d61a16ca065ad2797a
mohammad salah <mohmmad.salah2002@gmail.com>
2023-12-30 02:09:34 +0300

    final map update

--- lib/campusmap.dart
+++ lib/campusmap.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: unnecessary_const
+
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -8,33 +12,73 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
+  var markersOnMap = HashSet<Marker>();
+  MapType _currentMapType = MapType.normal; // Default map type
 
-  final LatLng _universityLocation =
-      const LatLng(32.0995480065019, 36.1909090670292);
-  // Replace YOUR_LATITUDE and YOUR_LONGITUDE with your university's coordinates
+  static const LatLng _universityLocation =
+      LatLng(32.0995480065019, 36.1909090670292);
 
+  Set<Circle> circles = Set.from([
+    const Circle(
+      circleId: const CircleId('HUFocusCircl'),
+      center: LatLng(32.10101412124145, 36.18650692941451),
+      radius: 1000,
+      strokeWidth: 1,
+      fillColor: Color.fromARGB(80, 169, 228, 225),
+    )
+  ]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('University Map'),
       ),
-      body: GoogleMap(
-        onMapCreated: (GoogleMapController controller) {
-          mapController = controller;
-        },
-        initialCameraPosition: CameraPosition(
-          target: _universityLocation,
-          zoom: 15.0,
-        ),
-        markers: {
-          Marker(
-            markerId: const MarkerId('University'),
-            position: _universityLocation,
-            infoWindow: const InfoWindow(title: 'Your University'),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: (GoogleMapController controller) {
+              mapController = controller;
+              setState(() {
+                markersOnMap.add(
+                  const Marker(
+                    markerId: MarkerId('University'),
+                    position: _universityLocation,
+                    infoWindow: InfoWindow(
+                      title: 'Your University',
+                      snippet: "mohammad salah",
+                    ),
+                    icon: BitmapDescriptor.defaultMarker,
+                  ),
+                );
+              });
+            },
+            initialCameraPosition: const CameraPosition(
+              target: _universityLocation,
+              zoom: 15.0,
+            ),
+            markers: markersOnMap,
+            mapType: _currentMapType,
+            circles: circles,
           ),
-        },
+          Positioned(
+            top: 16.0,
+            right: 16.0,
+            child: FloatingActionButton(
+              onPressed: () {
+                _toggleMapType();
+              },
+              child: const Icon(Icons.layers),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void _toggleMapType() {
+    setState(() {
+      _currentMapType =
+          _currentMapType == MapType.normal ? MapType.hybrid : MapType.normal;
+    });
   }
 }
